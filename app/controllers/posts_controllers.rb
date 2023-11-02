c82fcb2980e6e3a878aadedfdf1f62b93a94a4e9
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-11-02 16:53:16 -0600

    Add controllers for posts and users

---
+++ app/controllers/posts_controllers.rb
@@ -0,0 +1,11 @@
+# app/controllers/posts_controller.rb
+class PostsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+end
