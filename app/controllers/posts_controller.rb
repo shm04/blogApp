113cf21ab8bb82e4dd636d9a6ee34c87ac238318
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-11-04 17:26:08 -0600

    add forms to add comments, posts and likes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :find_user, only: %i[index create]
+  before_action :find_post, only: [:show]
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
@@ -6,5 +9,29 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to user_post_path(@user, @post), notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:user_id])
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,19 @@
 
 Rails.application.routes.draw do
   root 'users#index'
+
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:create]
+      resources :likes, only: [:create, :destroy]
+
+      member do
+        post 'likes', to: 'likes#create'
+      end
+    end
   end
+
+  delete '/likes/:id', to: 'likes#destroy', as: 'like'
+
+  post '/comments', to: 'comments#create', as: 'create_comment'
 end
