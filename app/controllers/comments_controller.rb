60d5110bac29b1eb00b62f06af06826a7a322a79
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-11-04 17:28:21 -0600

    Fix rubocop offenses

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,21 @@
 class CommentsController < ApplicationController
-    def create
-      @post = Post.find(params[:post_id])
-      @user = @post.author
-      @comment = @post.comments.build(comment_params)
-      @comment.user = current_user
-  
-      if @comment.save
-        flash[:success] = 'Comment created successfully.'
-        redirect_to user_post_path(@user, @post)
-      else
-        flash[:error] = 'Comment could not be created.'
-      end
-    end
-  
-    private
-  
-    def comment_params
-      params.require(:comment).permit(:text)
+  def create
+    @post = Post.find(params[:post_id])
+    @user = @post.author
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      flash[:success] = 'Comment created successfully.'
+      redirect_to user_post_path(@user, @post)
+    else
+      flash[:error] = 'Comment could not be created.'
     end
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,33 +1,33 @@
 class LikesController < ApplicationController
-    def create
-      @post = Post.find(params[:post_id])
-      @like = @post.likes.build(like_params)
-  
-      if @like.save
-        flash[:success] = 'Liked post!'
-      else
-        flash[:error] = 'Failed to like post.'
-      end
-  
-      redirect_to user_post_path(@post.author, @post)
+  def create
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.build(like_params)
+
+    if @like.save
+      flash[:success] = 'Liked post!'
+    else
+      flash[:error] = 'Failed to like post.'
     end
-  
-    def destroy
-      @like = Like.find(params[:id])
-      @post = @like.post
-  
-      if @like.destroy
-        flash[:success] = 'Unliked post.'
-      else
-        flash[:error] = 'Failed to unlike post.'
-      end
-  
-      redirect_to user_post_path(@post.author, @post)
-    end
-  
-    private
-  
-    def like_params
-      params.require(:like).permit(:user_id)
-    end
+
+    redirect_to user_post_path(@post.author, @post)
   end
+
+  def destroy
+    @like = Like.find(params[:id])
+    @post = @like.post
+
+    if @like.destroy
+      flash[:success] = 'Unliked post.'
+    else
+      flash[:error] = 'Failed to unlike post.'
+    end
+
+    redirect_to user_post_path(@post.author, @post)
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:user_id)
+  end
+end
