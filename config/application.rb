5cacf1e633453ddbe7db4ca17afebb486b282076
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-11-11 16:34:23 -0600

    Add changes

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module BlogApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
