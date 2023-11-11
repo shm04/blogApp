988cbaa3b4272ce95da7b2716ef4016d5057aea1
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-11-11 16:18:05 -0600

    Add and config devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,21 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+  after_create :send_confirmation_instructions
   validates :name, presence: true
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   has_many :posts, foreign_key: :author_id, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def send_confirmation_instructions
+    puts "Confirmation Token: #{confirmation_token}"
+    puts "Confirmation Sent At: #{confirmation_sent_at}"
+    puts "Confirmed At: #{confirmed_at}"
+    puts "Unconfirmed Email: #{unconfirmed_email}"
+
+    # ... other code
+  end
 
   def recent_posts
     posts.includes(:comments).order(created_at: :desc).limit(3)
